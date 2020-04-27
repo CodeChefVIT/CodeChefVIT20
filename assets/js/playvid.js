c397c6c8be1024992eb423ebd056cd66a2cbaeb5
apoorvachawda <apoorvachawda121@gmail.com>
2020-04-27 17:42:28 +0530

    changed webinars page

--- assets/js/playvid.js
+++ assets/js/playvid.js
@@ -1,4 +1,21 @@
-// 2. This code loads the IFrame Player API code asynchronously.
+// var tag = document.createElement('script');
+//   tag.id = 'iframe-demo';
+//   tag.src = 'https://www.youtube.com/iframe_api';
+//   var firstScriptTag = document.getElementsByTagName('script')[0];
+//   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+//   var player;
+//   function onYouTubeIframeAPIReady() {
+//     player = new YT.Player('player', {
+//         events: {
+//           'onReady': onPlayerReady,
+//           'onStateChange': onPlayerStateChange
+//         }
+//     });
+//   }
+
+
+      // 2. This code loads the IFrame Player API code asynchronously.
       var tag = document.createElement('script');
 
       tag.src = "https://www.youtube.com/iframe_api";
