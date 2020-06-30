a8727dae76be2f6237a0210c77e04bbf05800230
apoorvachawda <apoorvachawda121@gmail.com>
2020-07-01 00:07:17 +0530

    updated validity check function

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,12 +3,9 @@ function validateForm(){
      var email = document.getElementById('email');
      var phone = document.getElementById('phone');
      var domain = document.getElementById('domain');
-     if (!(name.oninvalid)||(email.oninvalid)||(phone.oninvalid)||(domain.oninvalid))
-     {
-        alert("Please fill valid details !");
-        return false;
-     }
-     return true;
+     console.log(email.checkValidity());
+     console.log(phone.checkValidity());
+     return((email.checkValidity())&&(phone.checkValidity()));
 }
 function contact()
 {
@@ -25,14 +22,21 @@ function contact()
     
  
     xh.send(JSON.stringify(data))
-    xh.onload=function(){
-        if(!validateForm()){
-            window.location.replace('contact.html');
-        }
+     xh.onload=function(){
+        console.log('return value ' + validateForm());
+       if(!validateForm())
+       {
+        toastr.error('Enter valid details !');
+       }
+      
         else if(this.status==201)
         {
-            alert('Thanks for contacting!');
-            window.location.replace('contact.html');
+            toastr.success('Thanks for contacting !');
+            // window.location.replace('contact.html');
+        }
+        else{
+            toastr.error('Failed! Try again');
+            // window.location.replace('contact.html');
         }
 }
 }
