7f2b9a23f63029ce2bee683d10b951b7da91279a
Manasvi070902 <manasvi.alimchandani2019@vitstudent.ac.in>
2020-07-05 12:03:43 -0700

    minor changes

--- middleware/captcha.js
+++ middleware/captcha.js
@@ -2,13 +2,13 @@ var request=require('request')
 var verifyCaptcha = (req, res, next) => {
     bodydata = req.body
 
-    if (!bodydata['recaptcha']) {
+    if (!bodydata['g-recaptcha']) {
         return res.json({
             status: false
         });
     }
 
-    const token = bodydata['recaptcha'] || req.query['recaptcha'];
+    const token = bodydata['g-recaptcha'] || req.query['g-recaptcha'];
     const verificationUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.SECRETKEY}&response=${token}&remoteip=${req.connection.remoteAddress}`;
 
     request(verificationUrl, (error, response, body) => {
