65347f4a39cb42c650f596dff9a72bbd3c5fa4b1
NavyaaSharma <navyaasharma700@rediffmail.com>
2020-07-07 01:28:24 +0530

    set status

--- middleware/captcha.js
+++ middleware/captcha.js
@@ -15,11 +15,7 @@ var verifyCaptcha = (req, res, next) => {
         body = JSON.parse(body);
         if (body.success !== undefined && !body.success) {
             console.log(body)
-<<<<<<< HEAD
              res.status(401).json({
-=======
-            return res.status(401).json({
->>>>>>> a240473d14ae41c152dd1853884483feff92b809
                 status: false
             });
         
