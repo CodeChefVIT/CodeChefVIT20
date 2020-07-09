dcfcfa29b019c02d9bc78f84806febf25e8bd68d
Manasvi070902 <manasvi.alimchandani2019@vitstudent.ac.in>
2020-07-08 21:18:51 -0700

    trial

--- model/data.js
+++ model/data.js
@@ -11,6 +11,7 @@ const userSchema =new mongoose.Schema({
     email: {
             type:String,
             required:true,
+            unique: true,
              match: /[a-z0-9!#$%&'+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'+/=?^_`{|}~-]+)@(?:[a-z0-9](?:[a-z0-9-][a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
     
 },
