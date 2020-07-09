ca9926a4b927633a1fd4f4d294898b9152b6a0d2
NavyaaSharma <navyaasharma700@rediffmail.com>
2020-07-09 17:22:09 +0530

    fix

--- app.js
+++ app.js
@@ -56,6 +56,6 @@ app.post('/contact',captcha,(req, res) => {
 
 
 
-  const PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
